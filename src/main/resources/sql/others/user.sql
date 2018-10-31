98bfb9a025f21e156b82c190b8d89a12242fa76d
mayingcheng <945600484@qq.com>
2018-10-31 14:49:49 +0800

    修复错误的SQL

--- src/main/resources/sql/others/user.sql
+++ src/main/resources/sql/others/user.sql
@@ -4,11 +4,12 @@ create table BASE_USER (
   COMMENT 'ID',
   USER_NAME     VARCHAR(50)  NOT NULL                 DEFAULT ''
   COMMENT '用户名称',
+  USER_PHONE       VARCHAR(200)  NOT NULL             DEFAULT ''
+  COMMENT '电话号码',
   USER_PASSWORD VARCHAR(200) NOT NULL                DEFAULT ''
   COMMENT '用户密码',
   STATUS        INT(2)       NOT NULL                DEFAULT 0
   COMMENT '状态',
   CREATE_AT     TIMESTAMP    NOT NULL                DEFAULT CURRENT_TIMESTAMP
   COMMENT '创建时间'
-)
 )
